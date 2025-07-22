89a3b1e4cba3ce1c74dc4a12f303327852d32dff
Nehad <35563834+81joker@users.noreply.github.com>
2025-07-22 21:33:57 +0200

    Import crude the product page

--- resources/views/filament/resources/products-resource/pages/list-products.blade.php
+++ resources/views/filament/resources/products-resource/pages/list-products.blade.php
@@ -1,9 +1,30 @@
 <x-filament-panels::page>
-
-<div class="container  py-8 border border-gray-200 rounded-md shadow-md">
-        <h1 class="text-2xl font-bold text-center m-auto text-red-600">Finden Sie den richtigen Plan für Ihr Kind und Sich</h1>
-        <p class="text-center text-gray-600 mt-4">Hier finden Sie eine Übersicht unserer verschiedenen Pläne, die auf die Bedürfnisse von Kindern und Eltern zugeschnitten sind.</p>
-
-</div>
-
+        <div class="container  border border-gray-200 rounded-md shadow-md py-6 px-6">
+                <x-filament::grid default="1" sm="2" lg="3" xl="4" class="gap-6">
+                        @foreach($this->products as $product)
+                        <x-filament::card class="flex flex-col h-full">
+                                <h5 class="text-start uppercase">{{ $product->title }}</h5>
+                                <div class="flex font-bold mb-4 pb-1">
+                                        <h1 class="text-blue-600/100 pr-1" style="font-size: 2rem;">€ {{ $product->price }}</h1>
+                                         <sub class="text-gray-500 mt-6 uppercase" style="font-size: 1rem;">/ {{ $product->term_month }} Monate</sub>
+                                </div>
+                                <p class="text-gray-600 mb-4 text-sm">
+                                {{ $product->description }}
+                                </p>
+                                <div class="mt-4 pt-4 border-t border-gray-200">
+                                <x-filament::button
+                                    type="button"
+                                    color="primary"
+                                    size="sm"
+                                    tag="a"
+                                    href="{{ route('filament.admin.resources.admin.products.edit', ['record' => $product->id]) }}"
+                                    class="w-full py-3">
+                                    Edit Product
+                                </x-filament::button>
+                            </div>
+                        </x-filament::card>
+                        @endforeach
+                </x-filament::grid>
+        </div>
 </x-filament-panels::page>
+<script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
